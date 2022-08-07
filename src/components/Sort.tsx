42bad995e2ef89482ff255d8d20c22bf9c34c5d7
Adel Absalyamov <adel.absalyamov@yandex.ru>
2022-08-07 15:02:20 +0300

    pagination && sort

--- src/components/Sort.tsx
+++ src/components/Sort.tsx
@@ -18,12 +18,12 @@ type SortPopupProps = {
 }
 
 export const sortList: SortItem[] = [
-  { name: 'популярности (DESC)', sortProperty: SortPropertyEnum.RATING_DESC },
-  { name: 'популярности (ASC)', sortProperty: SortPropertyEnum.RATING_ASC },
-  { name: 'цене (DESC)', sortProperty: SortPropertyEnum.PRICE_DESC },
-  { name: 'цене (ASC)', sortProperty: SortPropertyEnum.PRICE_ASC },
-  { name: 'алфавиту (DESC)', sortProperty: SortPropertyEnum.TITLE_DESC },
-  { name: 'алфавиту (ASC)', sortProperty: SortPropertyEnum.TITLE_ASC },
+  { name: 'популярности по убыванию', sortProperty: SortPropertyEnum.RATING_DESC },
+  { name: 'популярности по возрастанию', sortProperty: SortPropertyEnum.RATING_ASC },
+  { name: 'цене по убыванию', sortProperty: SortPropertyEnum.PRICE_DESC },
+  { name: 'цене по возрастанию', sortProperty: SortPropertyEnum.PRICE_ASC },
+  { name: 'алфавиту по убыванию', sortProperty: SortPropertyEnum.TITLE_DESC },
+  { name: 'алфавиту по возрастанию', sortProperty: SortPropertyEnum.TITLE_ASC },
 ];
 
 export const SortPopup: React.FC<SortPopupProps> = memo(({value}) => {
